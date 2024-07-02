b8c5f8c51e9cf46b772a1b78fc0569a7ebed1c27
huseyin <huseyintartar1907@gmail.com>
2024-07-03 02:27:07 +0300

    Ödev

---
+++ src/main/java/org/example/hafta2/ders2/odev/AsalSayiVerimli.java
@@ -0,0 +1,39 @@
+package org.example.hafta2.ders2.odev;
+
+import java.util.Scanner;
+
+public class AsalSayiVerimli {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int number = sc.nextInt();
+		long startTime = System.currentTimeMillis();
+		int counter = 0;
+		if (number % 2 == 0 && number != 2) {
+			System.out.println("This is not Prime Number.");
+		}
+		else if (number == 2 || number == 1) {
+			System.out.println("This is a Prime Number.");
+		}
+		else if (number < 1) {
+			System.out.println("This is not Prime Number");
+		}
+		else {
+			for (int i = 3; i <= number / 3; i += 2) {
+				if (number % i == 0) {
+					System.out.println("This is not Prime Number");
+					break;
+				}
+			}
+			if (counter == 0) {
+				System.out.println("This is a Prime Number.");
+			}
+			
+		}
+		long endTime = System.currentTimeMillis();
+		long elapsedTime = endTime - startTime;
+		System.out.println(elapsedTime + " : Calisma Suresi");
+	}
+	
+	
+}
---
+++ src/main/java/org/example/hafta2/ders2/odev/AsalSayiVerimsiz.java
@@ -0,0 +1,38 @@
+package org.example.hafta2.ders2.odev;
+
+import java.util.Scanner;
+
+public class AsalSayiVerimsiz {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int number = sc.nextInt();
+		long startTime = System.currentTimeMillis();
+		int counter = 0;
+		if (number == 2) {
+			System.out.println("This is a Prime Number.");
+		}
+		else if(number<1){
+			System.out.println("This is not Prime Number");
+		}
+		else if (number == 1) {
+			System.out.println("This is a Prime Number");
+		}
+		else {
+			for (int i = 1; i <= number; i++) {
+				if (number % i == 0) {
+					counter++;
+				}
+			}
+			if (counter <= 2) {
+				System.out.println("This is a Prime Number.");
+			}
+			else {
+				System.out.println("This is not a Prime Number.");
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		long elapsedTime = endTime - startTime;
+		System.out.println(elapsedTime + " : Calisma Suresi");
+	}
+}
