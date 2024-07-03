b12e9953299da2dc131a47bdefffc183100a7a2e
huseyin <huseyintartar1907@gmail.com>
2024-07-03 10:48:04 +0300

    ...

--- src/main/java/org/example/hafta2/ders2/odev/AsalSayiVerimli.java
+++ src/main/java/org/example/hafta2/ders2/odev/AsalSayiVerimli.java
@@ -8,34 +8,32 @@ public class AsalSayiVerimli {
 		System.out.print("Enter a number : ");
 		int number = sc.nextInt();
 		long startTime = System.currentTimeMillis();
-		int counter = 0;
-		if (number % 2 == 0 && number != 2) {
-			System.out.println("This is not Prime Number.");
-		}
-		else if (number == 2 || number == 1) {
+		
+		if (number <= 1) {
+			System.out.println("This is not a Prime Number.");
+		} else if (number == 2) {
 			System.out.println("This is a Prime Number.");
-		}
-		else if (number < 1) {
-			System.out.println("This is not Prime Number");
-		}
-		else {
-			for (int i = 3; i <= number / 3; i += 2) {
+		} else if (number % 2 == 0) {
+			System.out.println("This is not a Prime Number.");
+		} else {
+			boolean isPrime = true;
+			for (int i = 3; i <= Math.sqrt(number); i += 2) {
 				if (number % i == 0) {
-					System.out.println("This is not Prime Number");
-					counter++;
+					isPrime = false;
 					break;
 				}
-				if (counter == 0) {
-					System.out.println("This is a Prime Number.");
-				}
 			}
-
-			
+			if (isPrime) {
+				System.out.println("This is a Prime Number.");
+			} else {
+				System.out.println("This is not a Prime Number.");
+			}
 		}
+		
 		long endTime = System.currentTimeMillis();
 		long elapsedTime = endTime - startTime;
-		System.out.println(elapsedTime + " : Calisma Suresi");
+		System.out.println(elapsedTime + " ms : Execution Time");
+		
+		sc.close();
 	}
-	
-	
 }
