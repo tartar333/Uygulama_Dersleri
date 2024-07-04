f074bc729ee55ca9304bbd8f4ac401daa165340f
huseyin <huseyintartar1907@gmail.com>
2024-07-04 04:51:55 +0300

    ...

--- src/main/java/org/example/hafta2/ders1/Soru5.java
+++ src/main/java/org/example/hafta2/ders1/Soru5.java
@@ -6,13 +6,12 @@ public class Soru5 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int b=1;
-		int c=2;
 		System.out.print("Faktoriyel hesaplayacaginiz sayiyi girin : ");
 		int a= scanner.nextInt();
 		for(int i=2; i<=a; i++) {
 			
-			b=b*c;
-			c++;
+			b=b*i;
+			
 		}
 		System.out.println("Girilen sayinin faktoriyeli = "+b+"'dir");
 	}
---
+++ src/main/java/org/example/hafta2/ders3/Odev/Menu.java
@@ -0,0 +1,124 @@
+package org.example.hafta2.ders3.Odev;
+
+import java.util.Scanner;
+
+public class Menu {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hosgeldiniz!/Welcome!");
+		System.out.print("Lutfen goruntuleme dilini seciniz. / Please select display language.\n 1--Turkish(TR) / " +
+				                 "Turkce icin '1'e basin" +
+				                 " \n " + "2--English(EN)/For English press number '2'\n ");
+		int select = sc.nextInt();
+		if (select == 1) {
+			while (select == 1) {
+				System.out.println("1-- Merhaba Dunya yazdir.\n" + "2-- Faktoriyel Hesapla.\n" + "3-- Asal sayi " +
+						                   "kontrolu yap.\n" + "0-- Cikmak icin farkli bir tusa basin\n");
+				int tSelect = sc.nextInt();
+				switch (tSelect) {
+					case 1:
+						System.out.println("Merhaba Dunya");
+						break;
+					case 2:
+						System.out.print("Faktoriyelini istediginiz sayiyi girin : ");
+						int a = sc.nextInt();
+						int b = 1;
+						for (int i = 1; i <= a; i++) {
+							b = b * i;
+							
+						}
+						System.out.println("Girilen sayinin Faktoriyeli : " + b + "'dir.");
+						break;
+					case 3:
+						System.out.println("Bir sayi giriniz : ");
+						long number = sc.nextLong();
+						if (number == 2) {
+							System.out.println("Asal Sayidir.");
+						}
+						else if (number <= 1 || number % 2 == 0) {
+							System.out.println("Asal Sayi Degildir.");
+						}
+						else {
+							boolean isPrime = true;
+							for (long i = 3; i <= Math.sqrt(number); i += 2) { //karekök alma
+								if (number % i == 0) {
+									isPrime = false;
+									break;
+								}
+							}
+							if (isPrime) {
+								System.out.println("Asal Sayidir.");
+							}
+							else {
+								System.out.println("Asal Sayi Degildir.");
+							}
+						}
+						break;
+					default:
+						System.out.println("Farkli bir tusa bastiniz cikis yapiliyor...");
+						select = 5;
+						break;
+				}
+			}
+		}
+		if (select == 2){
+			while (select == 2) {
+				System.out.println("1-- Print 'Hello World!'.\n" + "2-- Calculate The Factorial.\n" + "3-- Do a Prime" +
+						                   " " +
+						                   "Number Check.\n" + "0-- Press different key to exit\n");
+				int tSelect = sc.nextInt();
+				switch (tSelect) {
+					case 1:
+						System.out.println("Hello World!");
+						break;
+					case 2:
+						System.out.print("Enter the number you want the factorial:\n ");
+						int a = sc.nextInt();
+						int b = 1;
+						for (int i = 1; i <= a; i++) {
+							b = b * i;
+							
+						}
+						System.out.println("Factorial of the entered number : " + b);
+						break;
+					case 3:
+						System.out.println("Enter Number : ");
+						long number = sc.nextLong();
+						if (number == 2) {
+							System.out.println("This is Prime Number.");
+						}
+						else if (number <= 1 || number % 2 == 0) {
+							System.out.println("This is not Prime Number");
+						}
+						else {
+							boolean isPrime = true;
+							for (long i = 3; i <= Math.sqrt(number); i += 2) { //karekök alma
+								if (number % i == 0) {
+									isPrime = false;
+									break;
+								}
+							}
+							if (isPrime) {
+								System.out.println("This is Prime Number.");
+							}
+							else {
+								System.out.println("This is not Prime Number.");
+							}
+						}
+						break;
+					default:
+						System.out.println("You pressed different key. You are logging out...");
+						select = 5;
+						break;
+				}
+			}
+		}
+		
+		
+		if (select !=1 && select !=2){
+			System.out.println("1 veya 2 tuslarina basmadiginiz icin program sonlandirildi.\nThe program was " +
+					                   "terminated because you did not press the 1 or 2 keys.\n ");
+		}
+	}
+}
